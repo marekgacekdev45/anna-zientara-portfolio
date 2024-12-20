ec07e6a4ea355f5233c2ddff925d14417b31d1d5
marekgacek45 <marekgacek45@gmail.com>
2024-12-20 13:38:38 +0100

    change type in project card and portfolio section

--- components/home/portfolio.tsx
+++ components/home/portfolio.tsx
@@ -1,48 +1,60 @@
-import ProjectCard from '@/components/project-card'
-import BaseLink from '@/components/base-link'
+import ProjectCard from '@/components/project-card';
+import BaseLink from '@/components/base-link';
 
-const projects = [
-	{
-		id: '1',
-		title: 'Projekt nr 1',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
-	},
-	{
-		id: '2',
-		title: 'Projekt nr 2',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
-	},
-	{
-		id: '3',
-		title: 'Projekt nr 3',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
-	},
-	{
-		id: '4',
-		title: 'Projekt nr 4',
-		description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
-	},
-]
+interface Project {
+	id: string;
+	title: string;
+	description: string;
+	slug?: string;
+  }
 
-// const Projects = (props:{projects:Project[]}) => {
-const Projects = () => {
-	return (
-		<section className='section pt-12 pb-12'>
-			<div className='wrapper '>
-				<div className='space-y-5 '>
-					<span className='preheading'>Moje</span>
-					<h2 className='heading'>Projekty</h2>
-				</div>
+const projects: Project[] = [
+  {
+    id: '1',
+    title: 'Projekt nr 1',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
+    slug: 'projekt-nr-1',
+  },
+  {
+    id: '2',
+    title: 'Projekt nr 2',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
+    slug: 'projekt-nr-2',
+  },
+  {
+    id: '3',
+    title: 'Projekt nr 3',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
+    slug: 'projekt-nr-3',
+  },
+  {
+    id: '4',
+    title: 'Projekt nr 4',
+    description: 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Velit ratione blanditiis quo?',
+    slug: 'projekt-nr-4',
+  },
+];
 
-				<div className='flex gap-y-16 gap-x-10 justify-center items-center flex-wrap py-12'>
-					{projects.map(project => (
-						<ProjectCard key={project.title} project={project} />
-					))}
-					<BaseLink href='/portfolio' className='after:bg-black dark:after:bg-white'>Zobacz wszystkie projekty</BaseLink>
-				</div>
-			</div>
-		</section>
-	)
-}
+const Portfolio = () => {
+  return (
+    <section className="section pt-12 pb-12">
+      <div className="wrapper">
+        <div className="space-y-5">
+          <span className="preheading">Moje</span>
+          <h2 className="heading">Projekty</h2>
+        </div>
 
-export default Projects
+        <div className="flex gap-y-16 gap-x-10 justify-center items-center flex-wrap py-12">
+          {projects.map((project) => (
+            <ProjectCard key={project.id} project={project} />
+          ))}
+          <BaseLink href="/portfolio" className="after:bg-black dark:after:bg-white">
+            Zobacz wszystkie projekty
+          </BaseLink>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Portfolio;
